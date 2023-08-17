3c84350147ee579c2d92878a15fe0e91d57d917d
yuanguangshan <yuanguangshan@gmail.com>
2023-08-17 16:50:29 +0800

    add a blog @ 2023-08-17

--- copy_blogs.sh
+++ copy_blogs.sh
@@ -17,6 +17,8 @@ echo "执行home代码中……"
   # 在这里添加代码段 c 的具体内容
 source_dir="/Users/yuangs/Library/Mobile Documents/iCloud~app~cyan~taio/Documents/Editor"  # 将 "/path/to/a" 替换为实际的a文件夹路径
 destination_dir="/Users/yuangs/gitblog/blog/_posts"  # 将 "/path/to/b" 替换为实际的b文件夹路径
+
+
 fi
 
 #自动拷贝blog文章，并检查有变化，自动git提交
@@ -35,6 +37,7 @@ for file in "$source_dir"/*; do
 done
 
 # 检查是否有文件变更
+
 git_status=$(git status --porcelain)
 if [[ -n $git_status ]]; then
     # 有文件变更，执行Git指令
